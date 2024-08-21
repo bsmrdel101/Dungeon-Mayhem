a0fedc6812f44208aac198dd9db5a348fa250287
bsmrdel101 <fireknights318@gmail.com>
2024-08-21 14:02:52 -0400

    chore: Fix player positions

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("Table")]
+    [SerializeField] private Transform[] _playerPositions;
+
+    [Header("References")]
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private PhotonView _view;
+
+
+    private void Start()
+    {
+        UpdatePlayerList();
+    }
+
+    private void UpdatePlayerList()
+    {
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            // if (PhotonNetwork.PlayerList[i].NickName != PhotonNetwork.LocalPlayer.NickName)
+            // {
+            //     Instantiate(_playerPrefab, _playerPositions[i]);
+            // }
+            Instantiate(_playerPrefab, _playerPositions[i]);
+        }
+    }
+}
