58b93c47dc40ad55fbb56f0bddcab2a13d078462
bsmrdel101 <fireknights318@gmail.com>
2024-08-22 09:09:03 -0400

    feat: Create PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Stats")]
+    private int _health = 10;
+}
