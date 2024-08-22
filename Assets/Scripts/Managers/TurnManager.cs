2fe131c54f8ab888113f179cd1c1d719f18a7a74
Gaber Player <gabertheplayer@gmail.com>
2024-08-22 08:40:14 -0500

    handleturns

--- Assets/Scripts/Managers/TurnManager.cs
+++ Assets/Scripts/Managers/TurnManager.cs
@@ -1,29 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
+using TMPro;
 using UnityEngine;
 
 public class TurnManager : MonoBehaviourPunCallbacks
 {
     [Header("Turns")]
     private Player[] _playerList = PhotonNetwork.PlayerList;
-    private int currentTurn = 0;
+    private int _currentTurn = 0;
 
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI _playerName;
+ 
 
     private void Start()
     {
-        // TODO: The current turn should be random
-
+        _currentTurn = Random.Range(0, _playerList.Length - 1);
         HandleTurn();
     }
 
     private void HandleTurn()
     {
-        // Display whos turn it is
+        _playerName.text = _playerList[_currentTurn].NickName;
 
         // Can end turn
 
         // Move currentTurn value
+    }
+
+    public void OnClick_EndTurn()
+    {
+        _currentTurn += 1;
+        if (_currentTurn > _playerList.Length)
+        {
+            _currentTurn = 0;
+        }
     }
 }
